77b34767ff129cc7a55c94f7145e559c4e39ec65
luixto <luisprz93@gmail.com>
2022-02-28 20:54:14 -0600

    WoRkIng wOrKiNg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
-
+const path = requiere('path');
 //Initializations
 const app = express();
 
 //Setting
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultlayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'), 
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
 
 //Middleware
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Global Variables
+app.use((req, res, next) => {
+
+    next()
+});
 
 //Routes
 app.use(requiere('./routes'));
