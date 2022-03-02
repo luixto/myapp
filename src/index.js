024b0f2ff296ee14a1ce30d6221173ebe3763377
luixto <luisprz93@gmail.com>
2022-03-01 20:26:47 -0600

    Adding all the files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-const path = requiere('path');
+const morgan =  require('morgan');
+const exphbs =  require('express-handlebars');
+const path =    require('path');
 //Initializations
 const app = express();
 
 //Setting
 app.set('port', process.env.PORT || 4000);
+
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultlayout: 'main',
@@ -31,6 +32,9 @@ app.use((req, res, next) => {
 
 //Routes
 app.use(requiere('./routes'));
+app.use(requiere('./routes/authentication'));
+app.use('links', requiere('./routes/links'));
+
 
 //Public
 
---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+    host: 'localhost',
+    user: 'debian-sys-maint',
+    password: 'BIwt2sL28rxiS3bg',
+    database: 'database_links'
+}
+
+};
